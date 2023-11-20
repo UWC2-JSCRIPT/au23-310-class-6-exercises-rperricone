77d4176a5d45f210c9ae0e85c878cc745a3fa794
Robin <robinp31@uw.eduL>
2023-11-19 18:08:16 -0700

    Completed HW

--- carProto.js
+++ carProto.js
@@ -5,7 +5,29 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+class Car {
+    constructor(model) {
+        this.currentSpeed = 0;
+        this.model = model
+    }
+    accelerate() {
+        this.currentSpeed++;
+    }
+    brake(){
+        this.currentSpeed--;
+    }
+    toString() {
+        return `Car model ${this.model} has a current speed of ${this.currentSpeed}`;
+    }
+}
 
+//  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+const jeep = new Car('Jeep');
+
+jeep.accelerate();
+jeep.accelerate();
+jeep.brake();
+console.log(jeep.toString());
 /**
  * ElectricCar class
  * @constructor
@@ -13,3 +35,50 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+class Radio{
+    constructor(){
+    this.on = false
+
+    }
+    isOn(){
+        console.log("radio ison?:"+this.on)
+        return this.on
+    }
+    switchOn(on){
+        console.log("switching car radio:"+on)
+        this.on = on
+        
+    }
+}
+class Electric extends Car{
+    constructor(model){
+        super(model)
+        this.motor = "electric"
+        this.radio = new Radio()
+
+    }
+    isRadioOn(){
+        return this.radio.isOn()
+    }
+    switchOnRadio(on){
+     return this.radio.switchOn(on)
+    }
+    accelerate(){
+        super.accelerate()// 1
+        super.accelerate()// 2
+    }
+    toString() {
+        return `This really cool electric car ${this.model} has a current speed of ${this.currentSpeed}`;
+    }
+}
+
+const tesla = new Electric('Tesla');
+
+tesla.accelerate();
+tesla.accelerate();
+tesla.brake();
+console.log(tesla.toString());
+tesla.isRadioOn()
+tesla.switchOnRadio(true)
+tesla.isRadioOn()
+
--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -9,6 +9,46 @@
 /**
  * Adds new list item to <ul>
  */
+
+
+$(document).ready(function (){
+
+  //if li element clicked 
+  //  toggle class done (done) 
+    //if delete a clicked remove the li
+    //if select all thwe class delete
+
+    //if add linked is clicked add item as new list item to the list
+
+
+   
+function addEventItems(){
+  let deleters = $('.delete')
+  for(deleteThing of deleters){
+    
+  $(deleteThing).on('click',(event)=>{
+    //console.log($(event.target), 'deletething')
+    //console.log('deleeting')
+    $(event.target).parent().remove()
+
+  })
+}
+
+let li = $('main>ul').children()
+//console.log(li)
+li.on('click',(event)=>{
+ if(event.target.className==='done'){
+  event.target.className=''
+ }
+ else{
+  event.target.className='done'
+ }
+   //console.log(event.target, event.target.className)
+})
+
+}
+
+  
 const addListItem = function(e) {
   e.preventDefault();
   const text = $('input').val();
@@ -17,3 +57,15 @@ const addListItem = function(e) {
 };
 
 // add listener for add
+addEventItems()
+$("main>div>.add-item").on('click',()=>{ 
+      let ul = $('main>ul') 
+      const text = $('input').val();
+      ul.append(`<li>
+      <span>${text}</span>
+      <a class="delete">Delete</a>
+    </li>`)
+    $('input').val('item')
+    addEventItems() 
+})
+})
